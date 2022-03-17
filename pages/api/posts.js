f90cbe677ecdb5b216fd2fe6d784d611d149c137
Daniel <LordHippopotamus@yandex.ru>
2022-03-17 12:08:49 +0300

    feat: create posts api route

---
+++ pages/api/posts.js
@@ -0,0 +1,20 @@
+import Cosmic from "cosmicjs";
+import { bucketSlug, readKey } from "../../lib/keys";
+
+const api = Cosmic();
+
+const postsBucket = api.bucket({
+  slug: bucketSlug,
+  read_key: readKey,
+});
+
+export default async function handler(req, res) {
+  const data = await postsBucket.getObjects({
+    query: {
+      type: "posts",
+    },
+    props: "slug,title,content,metadata",
+  });
+  const posts = await data.objects;
+  res.status(200).json({ posts });
+}
